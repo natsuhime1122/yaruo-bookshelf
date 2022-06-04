2ebf12df9b111b1e168ab61a414546b4a42ffeb9
nakayama_keita <natsuhime1122@gmail.com>
2022-06-05 00:33:00 +0900

    BootstrapVue追加,prettier削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,20 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import AdminLayout from "@/layouts/AdminLayout.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AdminLayout,
+    children: [
+      {
+        path: "/",
+        name: "top",
+        component: () => import("../views/Top.vue"),
+      },
+    ],
   },
 ];
 
